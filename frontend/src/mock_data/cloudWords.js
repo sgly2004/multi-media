ba528eba477844c3e61ed88055033fa7fc7fdfca
aliyun <aly@example.com>
2024-12-26 12:07:31 +0800

    增加了词云

--- frontend/src/mock_data/cloudWords.js
+++ frontend/src/mock_data/cloudWords.js
@@ -6,49 +6,189 @@ export const mockCloudWords = {
     },
     {
       "text": "树",
-      "size": 4
+      "size": 5
     },
     {
       "text": "花",
-      "size": 3
+      "size": 5
     },
     {
       "text": "山",
-      "size": 2
+      "size": 5
     },
     {
       "text": "叶子",
-      "size": 2
+      "size": 5
     },
     {
       "text": "蝴蝶",
-      "size": 2
+      "size": 5
     },
     {
       "text": "房屋",
-      "size": 2
+      "size": 5
     },
     {
       "text": "鸟",
-      "size": 2
+      "size": 5
     },
     {
       "text": "船",
-      "size": 2
+      "size": 5
     },
     {
       "text": "柳树",
+      "size": 5
+    },
+    {
+      "text": "房子",
+      "size": 4
+    },
+    {
+      "text": "鸭子",
+      "size": 3
+    },
+    {
+      "text": "牛",
+      "size": 3
+    },
+    {
+      "text": "石头",
+      "size": 2
+    },
+    {
+      "text": "狗",
+      "size": 2
+    },
+    {
+      "text": "牡丹花",
+      "size": 2
+    },
+    {
+      "text": "孩子",
+      "size": 2
+    },
+    {
+      "text": "岩石",
+      "size": 2
+    },
+    {
+      "text": "桥",
+      "size": 2
+    },
+    {
+      "text": "桃树",
+      "size": 2
+    },
+    {
+      "text": "蔷薇花",
+      "size": 2
+    },
+    {
+      "text": "芦苇",
+      "size": 2
+    },
+    {
+      "text": "假山",
+      "size": 2
+    },
+    {
+      "text": "椅子",
+      "size": 2
+    },
+    {
+      "text": "蔷薇",
+      "size": 2
+    },
+    {
+      "text": "货物",
+      "size": 2
+    },
+    {
+      "text": "葡萄",
+      "size": 2
+    },
+    {
+      "text": "橘",
+      "size": 2
+    },
+    {
+      "text": "蜻蜓",
+      "size": 2
+    },
+    {
+      "text": "鸦",
+      "size": 2
+    },
+    {
+      "text": "鹌鹑",
+      "size": 2
+    },
+    {
+      "text": "桌子",
+      "size": 2
+    },
+    {
+      "text": "荷叶",
+      "size": 2
+    },
+    {
+      "text": "松树",
+      "size": 2
+    },
+    {
+      "text": "羊",
+      "size": 2
+    },
+    {
+      "text": "麻雀",
+      "size": 2
+    },
+    {
+      "text": "梅花",
+      "size": 2
+    },
+    {
+      "text": "竹子",
+      "size": 2
+    },
+    {
+      "text": "龙",
+      "size": 2
+    },
+    {
+      "text": "乌龟",
+      "size": 2
+    },
+    {
+      "text": "牡丹",
+      "size": 2
+    },
+    {
+      "text": "猴子",
+      "size": 2
+    },
+    {
+      "text": "床",
+      "size": 2
+    },
+    {
+      "text": "风筝",
+      "size": 2
+    },
+    {
+      "text": "马",
       "size": 2
     }
   ],
   "元朝": [
     {
       "text": "树",
-      "size": 5
+      "size": 2
     },
     {
       "text": "山",
-      "size": 3
+      "size": 2
     },
     {
       "text": "竹子",
@@ -74,38 +214,290 @@ export const mockCloudWords = {
     },
     {
       "text": "树",
-      "size": 2
+      "size": 5
     },
     {
       "text": "岩石",
-      "size": 2
+      "size": 5
     },
     {
       "text": "房屋",
-      "size": 2
+      "size": 5
     },
     {
       "text": "花",
-      "size": 2
+      "size": 5
     },
     {
       "text": "竹子",
-      "size": 2
+      "size": 5
     },
     {
       "text": "荷叶",
-      "size": 2
+      "size": 5
     },
     {
       "text": "山",
-      "size": 2
+      "size": 5
     },
     {
       "text": "鸟",
-      "size": 2
+      "size": 5
     },
     {
       "text": "桥",
+      "size": 5
+    },
+    {
+      "text": "船",
+      "size": 5
+    },
+    {
+      "text": "梅花",
+      "size": 5
+    },
+    {
+      "text": "牛",
+      "size": 5
+    },
+    {
+      "text": "荷花",
+      "size": 5
+    },
+    {
+      "text": "松树",
+      "size": 4
+    },
+    {
+      "text": "青梅",
+      "size": 3
+    },
+    {
+      "text": "盆栽",
+      "size": 3
+    },
+    {
+      "text": "马",
+      "size": 2
+    },
+    {
+      "text": "假山",
+      "size": 2
+    },
+    {
+      "text": "蝴蝶",
+      "size": 2
+    },
+    {
+      "text": "鱼",
+      "size": 2
+    },
+    {
+      "text": "芭蕉叶",
+      "size": 2
+    },
+    {
+      "text": "柳树",
+      "size": 2
+    },
+    {
+      "text": "蒲扇",
+      "size": 2
+    },
+    {
+      "text": "浮萍",
+      "size": 2
+    },
+    {
+      "text": "凳子",
+      "size": 2
+    },
+    {
+      "text": "鹤",
+      "size": 2
+    },
+    {
+      "text": "水壶",
+      "size": 2
+    },
+    {
+      "text": "兔",
+      "size": 2
+    },
+    {
+      "text": "荔枝",
+      "size": 2
+    },
+    {
+      "text": "燕",
+      "size": 2
+    },
+    {
+      "text": "鸳鸯",
+      "size": 2
+    },
+    {
+      "text": "菊花",
+      "size": 2
+    },
+    {
+      "text": "八哥",
+      "size": 2
+    },
+    {
+      "text": "莲花",
+      "size": 2
+    },
+    {
+      "text": "莲蓬",
+      "size": 2
+    },
+    {
+      "text": "长缨",
+      "size": 2
+    },
+    {
+      "text": "芙蓉",
+      "size": 2
+    },
+    {
+      "text": "兰花",
+      "size": 2
+    },
+    {
+      "text": "鹰",
+      "size": 2
+    },
+    {
+      "text": "蜜蜂",
+      "size": 2
+    },
+    {
+      "text": "桂花",
+      "size": 2
+    },
+    {
+      "text": "绶带鸟",
+      "size": 2
+    },
+    {
+      "text": "团扇",
+      "size": 2
+    },
+    {
+      "text": "玉佩",
+      "size": 2
+    },
+    {
+      "text": "熊",
+      "size": 2
+    },
+    {
+      "text": "龙",
+      "size": 2
+    },
+    {
+      "text": "笛子",
+      "size": 2
+    },
+    {
+      "text": "凤",
+      "size": 2
+    },
+    {
+      "text": "扫把",
+      "size": 2
+    },
+    {
+      "text": "拂尘",
+      "size": 2
+    },
+    {
+      "text": "羊",
+      "size": 2
+    },
+    {
+      "text": "鸭",
+      "size": 2
+    },
+    {
+      "text": "柿子",
+      "size": 2
+    },
+    {
+      "text": "梨花",
+      "size": 2
+    },
+    {
+      "text": "竽",
+      "size": 2
+    },
+    {
+      "text": "雕像",
+      "size": 2
+    },
+    {
+      "text": "剑",
+      "size": 2
+    },
+    {
+      "text": "葫芦",
+      "size": 2
+    },
+    {
+      "text": "水果",
+      "size": 2
+    },
+    {
+      "text": "鹿",
+      "size": 2
+    },
+    {
+      "text": "鹅",
+      "size": 2
+    },
+    {
+      "text": "芦荻",
+      "size": 2
+    },
+    {
+      "text": "牡丹",
+      "size": 2
+    },
+    {
+      "text": "虾",
+      "size": 2
+    },
+    {
+      "text": "喜鹊",
+      "size": 2
+    },
+    {
+      "text": "蔷薇",
+      "size": 2
+    },
+    {
+      "text": "杏树",
+      "size": 2
+    },
+    {
+      "text": "驴",
+      "size": 2
+    },
+    {
+      "text": "梧桐",
+      "size": 2
+    },
+    {
+      "text": "白菜",
+      "size": 2
+    },
+    {
+      "text": "葵",
+      "size": 2
+    },
+    {
+      "text": "白鹭",
       "size": 2
     }
   ],
@@ -116,38 +508,250 @@ export const mockCloudWords = {
     },
     {
       "text": "人",
-      "size": 2
+      "size": 5
     },
     {
       "text": "山",
-      "size": 2
+      "size": 5
     },
     {
       "text": "房屋",
-      "size": 2
+      "size": 5
     },
     {
       "text": "岩石",
-      "size": 2
+      "size": 5
     },
     {
       "text": "荷叶",
-      "size": 2
+      "size": 5
     },
     {
       "text": "叶子",
-      "size": 2
+      "size": 5
     },
     {
       "text": "龙",
-      "size": 2
+      "size": 5
     },
     {
       "text": "孩子",
-      "size": 2
+      "size": 5
     },
     {
       "text": "柳树",
+      "size": 4
+    },
+    {
+      "text": "鸟",
+      "size": 4
+    },
+    {
+      "text": "松树",
+      "size": 3
+    },
+    {
+      "text": "桃树",
+      "size": 3
+    },
+    {
+      "text": "船",
+      "size": 3
+    },
+    {
+      "text": "荷花",
+      "size": 3
+    },
+    {
+      "text": "竹子",
+      "size": 2
+    },
+    {
+      "text": "芭蕉叶",
+      "size": 2
+    },
+    {
+      "text": "乌龟",
+      "size": 2
+    },
+    {
+      "text": "假山",
+      "size": 2
+    },
+    {
+      "text": "木偶",
+      "size": 2
+    },
+    {
+      "text": "莲花",
+      "size": 2
+    },
+    {
+      "text": "羊",
+      "size": 2
+    },
+    {
+      "text": "鸡",
+      "size": 2
+    },
+    {
+      "text": "蝴蝶",
+      "size": 2
+    },
+    {
+      "text": "梅花",
+      "size": 2
+    },
+    {
+      "text": "枇杷",
+      "size": 2
+    },
+    {
+      "text": "桥",
+      "size": 2
+    },
+    {
+      "text": "兔",
+      "size": 2
+    },
+    {
+      "text": "兔子",
+      "size": 2
+    },
+    {
+      "text": "砚台",
+      "size": 2
+    },
+    {
+      "text": "蒲扇",
+      "size": 2
+    },
+    {
+      "text": "菊花",
+      "size": 2
+    },
+    {
+      "text": "塔",
+      "size": 2
+    },
+    {
+      "text": "鹤",
+      "size": 2
+    },
+    {
+      "text": "狗尾草",
+      "size": 2
+    },
+    {
+      "text": "长缨",
+      "size": 2
+    },
+    {
+      "text": "牛",
+      "size": 2
+    },
+    {
+      "text": "宫灯",
+      "size": 2
+    },
+    {
+      "text": "蜡烛",
+      "size": 2
+    },
+    {
+      "text": "水壶",
+      "size": 2
+    },
+    {
+      "text": "团扇",
+      "size": 2
+    },
+    {
+      "text": "椅子",
+      "size": 2
+    },
+    {
+      "text": "凳子",
+      "size": 2
+    },
+    {
+      "text": "桌子",
+      "size": 2
+    },
+    {
+      "text": "水仙",
+      "size": 2
+    },
+    {
+      "text": "大蒜",
+      "size": 2
+    },
+    {
+      "text": "石榴花",
+      "size": 2
+    },
+    {
+      "text": "波罗蜜",
+      "size": 2
+    },
+    {
+      "text": "马",
+      "size": 2
+    },
+    {
+      "text": "虫",
+      "size": 2
+    },
+    {
+      "text": "针叶树",
+      "size": 2
+    },
+    {
+      "text": "蜻蜓",
+      "size": 2
+    },
+    {
+      "text": "海棠",
+      "size": 2
+    },
+    {
+      "text": "花",
+      "size": 2
+    },
+    {
+      "text": "鹅",
+      "size": 2
+    },
+    {
+      "text": "芦苇",
+      "size": 2
+    },
+    {
+      "text": "八哥",
+      "size": 2
+    },
+    {
+      "text": "猴子",
+      "size": 2
+    },
+    {
+      "text": "萝卜",
+      "size": 2
+    },
+    {
+      "text": "竹笋",
+      "size": 2
+    },
+    {
+      "text": "鞭炮",
+      "size": 2
+    },
+    {
+      "text": "荸荠",
+      "size": 2
+    },
+    {
+      "text": "菱角",
       "size": 2
     }
   ]
